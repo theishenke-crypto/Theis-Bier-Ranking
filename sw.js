bdc23c1102df6874ac4b8f9cc9ba115b633322f0
theishenke-crypto <theis.henke@gmail.com>
2026-03-05 17:20:07 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'beerlog-v1';
+const CACHE = 'beerlog-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
